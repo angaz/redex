e0e27da749970bb4a53142569ab3d85f582949b7
Angus Dippenaar <angusdippenaar@gmail.com>
2023-02-24 17:02:30 +0100

    update flake, build.zig

--- flake.nix
+++ flake.nix
@@ -1,34 +1,43 @@
 {
-  description = "";
+  description = "Redis server explorer. Explore the keys within your redis server as if it was a directory structure.";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    gitignore.url = "github:hercules-ci/gitignore.nix";
-    gitignore.inputs.nixpkgs.follows = "nixpkgs";
-
     flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    zigpkgs.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, gitignore, flake-utils }:
+  outputs = { self, nixpkgs, gitignore, flake-utils, zigpkgs }:
   let
     systems = [ "x86_64-linux" ];
     inherit (gitignore.lib) gitignoreSource;
   in flake-utils.lib.eachSystem systems (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+      zig = zigpkgs.packages.${system}.master;
     in rec {
       packages.default = packages.redex;
       packages.redex = pkgs.stdenvNoCC.mkDerivation {
         name = "redex";
         src = gitignoreSource ./.;
-        nativeBuildInputs = [ pkgs.zig ];
+        nativeBuildInputs = [zig];
         dontConfigure = true;
         dontInstall = true;
         buildPhase = ''
           mkdir -p "$out"
-          zig build install -Drelease-safe=true --prefix "$out"
+          zig build install -Doptimize=ReleaseSafe --prefix "$out"
         '';
         XDG_CACHE_HOME = ".cache";
+      };
+
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          zls
+        ] ++ [zig];
       };
     }
   );
